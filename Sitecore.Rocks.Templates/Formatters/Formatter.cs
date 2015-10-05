130cb5ff79d58fb8f990c4140914afa2cac9bdb6
Alex Willis <alex.willis@trueclarity.co.uk>
2015-10-05 17:33:16 +0100

    added mustache templating

--- Sitecore.Rocks.Templates/Formatters/Formatter.cs
+++ Sitecore.Rocks.Templates/Formatters/Formatter.cs
@@ -1,8 +1,32 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Mustache;
 using Sitecore.Rocks.Templates.Data;
 
 namespace Sitecore.Rocks.Templates.Formatters
 {
+    public class RemoveWhiteSpaceDefinition : InlineTagDefinition
+    {
+        public RemoveWhiteSpaceDefinition()
+            : base("removeWhiteSpace")
+        {
+        }
+
+        protected override IEnumerable<TagParameter> GetParameters()
+        {
+            return new[] { new TagParameter("string") };
+        }
+
+        public override void GetText(TextWriter writer, Dictionary<string, object> arguments, Scope context)
+        {
+            var str = (string)arguments["string"];
+            writer.Write(str.Replace(" ", string.Empty));
+        }
+    }
+
     public static class Formatter
     {
         private static string RemoveWhiteSpace(this string str)
@@ -12,10 +36,14 @@ namespace Sitecore.Rocks.Templates.Formatters
 
         public static string GetFieldSource(ISitecoreField field)
         {
-            return $@"
-    [SitecoreField(""{field.Name}"")]
-    public string {field.Name.RemoveWhiteSpace()} {{ get; set; }}
-";
+            var compiler = new FormatCompiler();
+            compiler.RegisterTag(new RemoveWhiteSpaceDefinition(), false);
+
+            var template = @"{{#newline}}
+    [SitecoreField(""{{Name}}"")]{{#newline}}
+    public string {{#removeWhiteSpace Name}} { get; set; }{{#newline}}";
+
+            return compiler.Compile(template).Render(field);
         }
 
         public static string GetModelSource(ISitecoreItem item)
